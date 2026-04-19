6423c56f7ad9a764fe6959409f6df18a47385b42
AK-giff <7alphasquad@gmail.com>
2026-04-19 21:20:51 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000" // change after deploy
+  baseURL: "https://your-render-url.onrender.com" // change after deploy
 });
