4ffbe40bad117ffe1e0b5f0a7dbfcf54446f6ad3
AK-giff <7alphasquad@gmail.com>
2026-04-19 23:03:06 +0530

    Update whatsapp.js

--- backend/services/whatsapp.js
+++ backend/services/whatsapp.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const sendMessage = async (phone, message) => {
   await axios.post(
-    https://graph.facebook.com/v18.0/${process.env.PHONE_NUMBER_ID}/messages,
+    `https://graph.facebook.com/v18.0/${process.env.PHONE_NUMBER_ID}/messages`,
     {
       messaging_product: "whatsapp",
       to: phone,
@@ -10,7 +10,7 @@ const sendMessage = async (phone, message) => {
     },
     {
       headers: {
-        Authorization: Bearer ${process.env.WHATSAPP_TOKEN},
+        Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
         "Content-Type": "application/json"
       }
     }
