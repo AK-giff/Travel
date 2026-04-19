8efc04472a0a16b2e10675940ae3ca03fc9fc681
AK-giff <7alphasquad@gmail.com>
2026-04-19 21:04:32 +0530

    Create LeadList.js

---
+++ frontend/src/components/LeadList.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import API from "../api";
+
+function LeadList({ onSelect, refresh }) {
+  const [leads, setLeads] = useState([]);
+
+  useEffect(() => {
+    fetchLeads();
+  }, [refresh]);
+
+  const fetchLeads = async () => {
+    const res = await API.get("/lead");
+    setLeads(res.data);
+  };
+
+  return (
+    <div>
+      {leads.map((lead) => (
+        <div
+          key={lead._id}
+          className="lead-card"
+          onClick={() => onSelect(lead)}
+        >
+          <strong>{lead.name || "Unknown"}</strong>
+          <br />
+          {lead.destination || "No destination"}
+          <br />
+          <small>Status: {lead.status}</small>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default LeadList;
